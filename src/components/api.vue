45ff24d39e1bbda8aa9458cb32b9ba61135adc99
xjm <445822667@qq.com>
2020-01-25 12:28:55 +0800

    'network-test-ok'

---
+++ src/components/api.vue
@@ -0,0 +1,30 @@
+<template>
+<h2>{{test}}</h2>
+</template>
+
+<script>
+    // import {getRankData} from '../networking/rank'
+    import {request} from "../networking/request";
+    export default {
+        name: "api",
+        data(){
+            return{
+                test:''
+            }
+        },
+        created() {
+            request({
+                url: '/paihangbang'
+            }).then(res => {
+                this.test=res
+                console.log(res);
+            }).catch(err => {
+                // console.log(err);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
