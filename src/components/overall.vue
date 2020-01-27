a1acaeb24c9c8350318f8dffefbff986a0747f13
xjm <445822667@qq.com>
2020-01-27 19:08:50 +0800

    overall70%

--- src/components/overall.vue
+++ src/components/overall.vue
@@ -11,18 +11,23 @@
             </Row>
 
             <div class="searchButton">
-                <!--            <Button type="primary" >查询数据</Button>-->
-                <!--            &nbsp;-->
-                <!--            &nbsp;-->
-                <Button type="success">导出Excel</Button>
+                            <Button type="primary" >查询数据</Button>
+                            &nbsp;
+                            &nbsp;
+                <Button type="success" style="margin-left: 15px">导出Excel</Button>
             </div>
             <br>
 
-            <row>
+            <Select class="picker1" placeholder="请选择游戏" v-model="model1" style="width:200px">
+                <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+
+            <row class="picker2">
                 <i-col span="12">
                     <Date-picker type="daterange" placement="right-start" placeholder="请选择日期范围" style="width: 200px"></Date-picker>
                 </i-col>
             </row>
+
     </div>
         <br>
         <div>
@@ -33,7 +38,7 @@
                 <Radio label="playtime">使用时长</Radio>
             </RadioGroup>
         </div>
-        <br>
+        <br><br>
         <row>
         <Card shadow>
             <div ref="dom" style="height: 400px;" ></div>
@@ -60,13 +65,29 @@
             return {
                 dom: null,
                 inforCardData: [
-                    { title: '新增用户', icon: 'md-person-add', count: 998, color: '#2d8cf0' },
-                    { title: '活跃用户', icon: 'md-person', count: 4998, color: '#19be6b' },
-                    { title: '启动次数', icon: 'md-locate', count: 142, color: '#ff9900' },
-                    { title: '使用时长', icon: 'md-share', count: 657, color: '#ed3f14' },
+                    { title: '新增用户(人)', icon: 'md-person-add', count: 998, color: '#2d8cf0' },
+                    { title: '活跃用户(人)', icon: 'md-person', count: 4998, color: '#19be6b' },
+                    { title: '启动次数(次)', icon: 'md-locate', count: 14200, color: '#ff9900' },
+                    { title: '使用时长(秒)', icon: 'md-share', count: 657, color: '#ed3f14' },
                     { title: '新增互动', icon: 'md-chatbubbles', count: 12, color: '#E46CBB' },
                     { title: '新增页面', icon: 'md-map', count: 14, color: '#9A66E4' }
                 ],
+                cityList: [
+                    {
+                        value: 'New York',
+                        label: 'New York'
+                    },
+                    {
+                        value: 'London',
+                        label: 'London'
+                    },
+                    {
+                        value: 'Sydney',
+                        label: 'Sydney'
+                    }
+
+                ],
+                model1: '',
             }
         },
         methods: {
@@ -107,55 +128,41 @@
                 ],
                 series: [
                     {
-                        name: '运营商/网络服务',
+                        name: '同比数据',
                         type: 'line',
-                        stack: '总量',
-                        areaStyle: { normal: {
-                                color: '#2d8cf0'
-                            } },
+                        // stack: '总量',
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'bottom',
+
+                            }
+                        },
+                        areaStyle: {
+                            normal: {
+                                color: '#FF4040',
+
+                            }
+                            },
                         data: [120, 132, 101, 134, 90, 230, 210]
                     },
+
                     {
-                        name: '银行/证券',
+                        name: '当前数据',
                         type: 'line',
-                        stack: '总量',
-                        areaStyle: { normal: {
-                                color: '#10A6FF'
-                            } },
-                        data: [257, 358, 278, 234, 290, 330, 310]
-                    },
-                    {
-                        name: '游戏/视频',
-                        type: 'line',
-                        stack: '总量',
-                        areaStyle: { normal: {
-                                color: '#0C17A6'
-                            } },
-                        data: [379, 268, 354, 269, 310, 478, 358]
-                    },
-                    {
-                        name: '餐饮/外卖',
-                        type: 'line',
-                        stack: '总量',
-                        areaStyle: { normal: {
-                                color: '#4608A6'
-                            } },
-                        data: [320, 332, 301, 334, 390, 330, 320]
-                    },
-                    {
-                        name: '快递/电商',
-                        type: 'line',
-                        stack: '总量',
+                        // stack: '总量',
                         label: {
                             normal: {
                                 show: true,
                                 position: 'top'
                             }
                         },
-                        areaStyle: { normal: {
-                                color: '#398DBF'
-                            } },
-                        data: [820, 645, 546, 745, 872, 624, 258]
+                        areaStyle: {
+                            normal: {
+                                color: '#1E90FF'
+                            }
+                            },
+                        data: [379, 268, 354, 269, 310, 478, 358]
                     }
                 ]
             }
@@ -178,11 +185,25 @@
 
     .searchButton{
         position: absolute ;
-        left:90%;
-        top:29%;
+        left:85%;
+        top:32.5%;
         z-index: 999;
     }
 
+
+    .picker2{
+        position: absolute ;
+        left:45%;
+        top:32.5%;
+        z-index: 999;
+    }
+
+    .picker1{
+        position: absolute ;
+        left:33%;
+        top:32.5%;
+        z-index: 999;
+    }
 
 </style>
 
